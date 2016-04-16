f3ad0017c2d6e8983baed642d93a5bc4cf6bb5d8
kristofereinarsson <kristofereinarsson@10.105.116.101>
2016-04-16 14:43:36 +0200

    Created a font for mikkel in the MainGui

--- src/gui/GuiData.java
+++ src/gui/GuiData.java
@@ -6,8 +6,14 @@ public class GuiData {
 	
 //This is a data class witch has all kind of data like fx. different fonts, sizes and stuff like that
 	
+//	The font for our login screen
 	public static Font getUserPassFont() {
 		return new Font("Myanmar Sangam MN", Font.BOLD, 18);
+	}
+	
+//	The font for the "corner profilepicture text" 
+	public static Font getCornerFont() {
+		return new Font("Myanmar Sangam MN", Font.PLAIN, 14);
 	}
 
 }
