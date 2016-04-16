115b5718c53f104caace81d51d5f835cbb699b9a
Mikkel Schmidt <Mikkel Schmidt@MikkelSchmidt>
2016-04-16 23:55:42 +0200

    df

--- src/gui/GuiData.java
+++ src/gui/GuiData.java
@@ -21,6 +21,10 @@ public class GuiData {
 		return new Font("Myanmar Sangam MN", Font.BOLD, 18);
 	}
 	
+	public static Font getLoginFieldFont() {
+		return new Font("Myanmar Sangam MN", Font.PLAIN, 18);
+	}
+	
 //	The font for the "corner profilepicture text" 
 	public static Font getCornerFont() {
 		return new Font("Myanmar Sangam MN", Font.PLAIN, 14);
