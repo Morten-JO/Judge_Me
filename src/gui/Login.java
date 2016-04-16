264766782984b9352e7521dfe4d91cabefb0f355
kristofereinarsson <kristofereinarsson@10.105.116.101>
2016-04-16 14:34:29 +0200

    Finished the loginPage

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -8,12 +8,13 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class Login {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField usernameTxtField;
+	private JPasswordField passwordTxtField;
 
 	/**
 	 * Launch the application.
@@ -49,23 +50,31 @@ public class Login {
 		JLabel username = new JLabel("Username: ");
 		username.setFont(GuiData.getUserPassFont());
 		username.setSize(username.getPreferredSize());
-		username.setBounds(40, 80, username.getWidth(), username.getHeight());
+		username.setBounds(60,70, username.getWidth(), username.getHeight());
 		frame.getContentPane().add(username);
 		
 		JLabel password = new JLabel("Password: ");
 		password.setFont(GuiData.getUserPassFont());
 		password.setSize(password.getPreferredSize());
-		password.setBounds(username.getX(), username.getY() + username.getHeight() + 20, password.getWidth(), password.getHeight());
+		password.setBounds(username.getX(), username.getY() + username.getHeight() + 30, password.getWidth(), password.getHeight());
 		frame.getContentPane().add(password);
 		
-		textField = new JTextField();
-		textField.setBounds(194, 78, 177, 28);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		usernameTxtField = new JTextField();
+		usernameTxtField.setBounds(username.getX() + username.getWidth() + 20, username.getY() - 10, 200, 40);
+		frame.getContentPane().add(usernameTxtField);
+		usernameTxtField.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(232, 136, 96, 28);
-		frame.getContentPane().add(passwordField);
+		passwordTxtField = new JPasswordField();
+		passwordTxtField.setBounds(password.getX() + username.getWidth() + 20, password.getY() - 10, 200, 40);
+		frame.getContentPane().add(passwordTxtField);
+		
+		JButton loginButton = new JButton("Login");
+		loginButton.setBounds(165, 182, 200, 40);
+		frame.getContentPane().add(loginButton);
+		
+		JButton btnCreateAccount = new JButton("Create account");
+		btnCreateAccount.setBounds(35, 182, 120, 40);
+		frame.getContentPane().add(btnCreateAccount);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
