d3e058cf7c00fdc521039447484fd7e7cd19476a
Mikkel Schmidt <Mikkel Schmidt@MikkelSchmidt>
2016-04-16 15:20:36 +0200

    gui

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -2,21 +2,23 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainGui extends JFrame {
+public class MainGui extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
+	private JButton genderMaleBtn, genderFemaleBtn;
 	private JLabel profileName, profilePicture;
-	Image img = getProfilePicture();
+	private Image img = getProfilePicture();
+	private Image judgingImg;
+	
 
 	/**
 	 * Launch the application.
@@ -54,7 +56,20 @@ public class MainGui extends JFrame {
 	void initialize(){
 		setLayout(null);
 		
-
+		genderMaleBtn = new JButton("Male");
+		genderMaleBtn.setFont(GuiData.getCornerFont());
+		genderMaleBtn.setSize(genderMaleBtn.getPreferredSize());
+		genderMaleBtn.setBounds(10, 10, genderMaleBtn.getWidth(), genderMaleBtn.getHeight());
+		genderMaleBtn.addActionListener(this);
+		add(genderMaleBtn);
+		
+		genderFemaleBtn = new JButton("Female");
+		genderFemaleBtn.setFont(GuiData.getCornerFont());
+		genderFemaleBtn.setSize(genderFemaleBtn.getPreferredSize());
+		genderFemaleBtn.setBounds(genderMaleBtn.getX() + genderMaleBtn.getWidth(), genderMaleBtn.getY(), genderFemaleBtn.getWidth(), genderFemaleBtn.getHeight());
+		genderFemaleBtn.addActionListener(this);
+		add(genderFemaleBtn);
+		
 		profilePicture = new JLabel("");
 		profilePicture.setIcon(new ImageIcon(img));
 		profilePicture.setSize(profilePicture.getPreferredSize());
@@ -62,16 +77,30 @@ public class MainGui extends JFrame {
 		add(profilePicture);
 		
 		profileName = new JLabel("PROFILENAME");
-		//profileName =;
+		profileName.setFont(GuiData.getCornerFont());
 		profileName.setSize(profileName.getPreferredSize());
-		profileName.setBounds(profilePicture.getX() - profileName.getWidth() - 15, 20, profileName.getWidth(), profileName.getHeight());
+		profileName.setBounds(profilePicture.getX() - profileName.getWidth() - 15, profilePicture.getY() + (profilePicture.getHeight()-profileName.getHeight())/2, profileName.getWidth(), profileName.getHeight());
 		add(profileName);
 	}
 	
 	private Image getProfilePicture() {
 		// TODO Auto-generated method stub
 		Image img =  new ImageIcon(this.getClass().getResource("/man.jpg")).getImage();
-		img = img.getScaledInstance(70, 70, Image.SCALE_DEFAULT);
+		img = img.getScaledInstance(60, 60, Image.SCALE_DEFAULT);
 		return img;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == genderMaleBtn){
+			genderFemaleBtn.setBackground(GuiData.getNeutralColor());
+			genderMaleBtn.setBackground(GuiData.getMaleColor());
+		} else if(e.getSource() == genderFemaleBtn){
+			genderFemaleBtn.setBackground(GuiData.getNeutralColor());
+			genderMaleBtn.setBackground(GuiData.getMaleColor());
+		}
+		
 	}
 }
